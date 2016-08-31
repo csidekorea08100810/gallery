7a2b81a8835eec99e6bd222322694b6bb7eb1d74
Wanki <www@cside.co.kr>
2016-08-31 18:03:37 +0900

    유저 가입시 최근 게시물 삭제
    - 가입시 에러 및 필요가 없어졌으므로 삭제합니다.

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'image', 'intro', 'phone', 'recent_articles'
+        'name', 'email', 'password', 'image', 'intro', 'phone'
     ];
 
     /**
